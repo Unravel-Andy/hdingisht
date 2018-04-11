6a3663707ae306578b558b8285f158173c3fc313
Unravel-Andy <36170790+Unravel-Andy@users.noreply.github.com>
2018-04-11 13:18:20 -0700

    Stable Version (#4)
    * [add] scripts for unravel azure application bootstrap
    
    * [update] script location
    
    * [fix] bug
    
    * [fix] extract path
    
    * [update/add] log / add custom script action to trigger setup script
    
    * [update] sensor deploy.sh log format
    [fix] setup.py wrong hive version
    
    * [update] uncomment update config
    
    * [fix] typo
    
    * [add] update config
    
    * [update] task name
    
    * [fix] typo
    
    * [fix] missing update config
    
    * [add] function comment

---
+++ on-premises/hdi_onpremises_bootstrap.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+################################################################################################
+# Unravel for HDInsight Bootstrap Script                                                       #
+#                                                                                              #
+# The bootstrap script log is located at /tmp/unravel                                          #
+################################################################################################
+
+[ -z "$TMP_DIR" ] && export TMP_DIR=/tmp/unravel
+if [ ! -d $TMP_DIR ]; then
+    mkdir -p $TMP_DIR
+    chmod a+rw $TMP_DIR
+fi
+
+SCRIPT_URL="https://raw.githubusercontent.com/Unravel-Andy/hdingisht/test/on-premises/hdi_onpremises_setup.py"
+
+echo -e "\nDownloading Setup Script\n"
+wget -O $TMP_DIR/hdi_onpremises_setup.py $SCRIPT_URL
+if [ $? -eq 0 ];then
+    echo -e "\nDownload Successed\n"
+    sleep 5
+    echo -e "\nRunning Setup Script in the background\n"
+    nohup python $TMP_DIR/hdi_onpremises_setup.py > $TMP_DIR/hdi_onpremises_setup.log 2>$TMP_DIR/hdi_onpremises_setup.err &
+else
+    echo -e "\nDownload Failed\n"
+    exit 1
+fi
